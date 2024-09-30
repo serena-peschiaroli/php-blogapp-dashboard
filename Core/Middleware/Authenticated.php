5ad82947b5beb16934dffd9ec44c83d1c210ded6
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-09-30 12:32:15 +0200

    Add: Middleware folder, autheticated and guest classes

---
+++ Core/Middleware/Authenticated.php
@@ -0,0 +1,11 @@
+<?php
+namespace Core\Middleware;
+
+class Autheticated{
+    public function handle(){
+        if(!$_SESSION['user'] ?? false){
+            header('location: /');
+            exit();
+        }
+    }
+}
