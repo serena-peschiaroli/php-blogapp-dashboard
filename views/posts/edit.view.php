ddd73a0e2991dbbda426b0153ccb0439af486e2d
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-10-02 17:47:05 +0200

    fix: delete btn

--- views/posts/edit.view.php
+++ views/posts/edit.view.php
@@ -7,7 +7,7 @@
 
 
     <div class="container my-5">
-        <a href="posts/index" class="btn btn-success mt-3 mb-5">Back</a>
+        <a href="/posts" class="btn btn-success mt-3 mb-5">Back</a>
 
 
         <form method="POST" action="/post">
@@ -19,10 +19,10 @@
                     <input type=" text" class="form-control"
                         name=" title"
                         id="title"
-                        placeholder="Title of your post...">
-                    <?= $_POST['title'] ?? '' ?></input>
+                        value="<?= $post['title'] ?? '' ?>">
+
                     <?php if (isset($errors['title'])) : ?>
-                        <p class="text-danger text-center fs-6"><?= $errors['body'] ?></p>
+                        <p class="text-danger text-center fs-6"><?= $errors['title'] ?></p>
                     <?php endif; ?>
                 </div>
 
@@ -31,7 +31,7 @@
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label" for="body">Body:</label>
                 <div class="col-sm-6">
-                    <textarea class="form-control" name="body" id="body"><?= $_POST['body'] ?? '' ?></textarea>
+                    <textarea class="form-control" name="body" id="body"><?= $post['body'] ?? '' ?></textarea>
 
                     <?php if (isset($errors['body'])) : ?>
                         <p class="text-danger text-center fs-6"><?= $errors['body'] ?></p>
@@ -39,27 +39,43 @@
                 </div>
             </div>
 
+
+
+            <div class="row mb-3">
+
+                <div class="col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/posts" role="button">Cancel</a>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <button type="button" class="btn btn-danger delete-btn" onclick="deletePost()">Delete this post</button>
+
+
+                </div>
+
+
+            </div>
+        </form>
+
+        <form id="delete-form" class="d-none" method="POST" action="/post">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="id" value="<?= $post['id'] ?>">
+        </form>
+
     </div>
 
-        <div class="row mb-3">
-
-            <div class="offset-sm-3 col-sm-3 d-grid">
-                <button type="submit" class="btn btn-primary">Update</button>
-            </div>
-            <div class="col-sm-3 d-grid">
-                <a class="btn btn-outline-primary" href="/" role="button">Cancel</a>
-            </div>
-            <button type="button" class="btn btn-danger" onclick="document.querySelector('#delete-form').submit()">Delete this post</button>
-        </div>
-    </form>
-
-    <form id="delete-form" class="hidden" method="POST" action="/note">
-        <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="id" value="<?= $post['id'] ?>">
-    </form>
-
-    </div>
 
 </main>
 
+
 <?php require base_path('views/partials/footer.php') ?>
+
+<script type="text/javascript">
+    function deletePost() {
+        if (confirm('Are you sure you want to delete this post?')) {
+            document.getElementById('delete-form').submit();
+        }
+    }
+</script>
