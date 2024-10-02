40aedf1c523addd74a187485ff397b8144eaa786
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-10-02 17:45:38 +0200

    fixed: destroy controller

--- Http/controllers/posts/destroy.php
+++ Http/controllers/posts/destroy.php
@@ -1,6 +1,22 @@
 <?php
 
-view("notes/create.view.php", [
-    'heading' => 'Create Note',
-    'errors' => []
+use Core\App;
+use Core\Database;
+use Core\Session;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = Session::get('user')['id'];
+
+$post = $db->query('SELECT * from posts WHERE id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+authorize($post['author_id'] === $currentUserId);
+
+$db->query('DELETE from posts WHERE id = :id', [
+    'id' => $_POST['id']
 ]);
+
+header('location: /posts');
+exit();
